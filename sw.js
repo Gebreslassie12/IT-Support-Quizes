d5a5f6de94cd4fad245b9e0c003647e8dc66ccbb
Gebreslassie Taddesse <gebreslassietaddesse12@gmail.com>
2026-06-02 08:25:00 +0300

    This helps students

--- sw.js
+++ sw.js
@@ -1,4 +1,40 @@
-self.addEventListener('fetch', function(event) {
-  // This allows your app to run fast and load efficiently
+const CACHE_NAME = 'it-quiz-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 1. Download and save the files when the app is installing
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting(); // Force the new service worker to activate immediately
 });
 
+// 2. Clean up any old caches if you update the app later
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 3. Intercept network requests: Try cache first, then try the internet
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
